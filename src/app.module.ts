dc8b802910f5f957bc763c32cf200279bb990854
Hoang1408 <hoang1482002@gmail.com>
2022-09-08 23:18:18 +0700

    use database url

--- src/app.module.ts
+++ src/app.module.ts
@@ -54,11 +54,17 @@ import { UserModule } from './user/user.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      ...(process.env.DATABASE_URL
+        ? {
+            url: process.env.DATABASE_URL,
+          }
+        : {
+            host: process.env.DATABASE_HOST,
+            port: +process.env.DATABASE_PORT,
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+          }),
       entities: [User, Verification, Car, CarType, Booking],
       synchronize: process.env.NODE_ENV !== 'prod',
       logging:
